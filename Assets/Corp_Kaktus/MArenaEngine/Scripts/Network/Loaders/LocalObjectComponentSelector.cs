ab0d9848116fc376d34ea8534b9c7afe25c0c187
Djastas <109723332+Djastas@users.noreply.github.com>
2025-03-09 21:53:03 +0500

    start change system to role Selector
    LocalObjectComponentSelector
    LocalObjectComponentSelector

---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/Loaders/LocalObjectComponentSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Corp_Kaktus.MArenaEngine.Scripts.Network.RoleControl;
+using UnityEngine;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Network.Loaders
+{
+    public class LocalObjectComponentSelector : MonoBehaviour
+    {
+        [SerializeField] private List<Behaviour> onlyClientComponents;
+        [SerializeField] private List<Behaviour> onlyConsoleComponents;
+        [SerializeField] private List<Behaviour> onlyServerComponents;
+
+        private void Awake()
+        {
+            if (RoleController.currentRole != Role.Client) { DestroyAll(onlyClientComponents); }
+            if (RoleController.currentRole != Role.Console) { DestroyAll(onlyConsoleComponents); }
+            if (RoleController.currentRole != Role.Server) { DestroyAll(onlyServerComponents); }
+        }
+
+        private static void DestroyAll(List<Behaviour> behaviours)
+        { foreach (var behaviour in behaviours) { Destroy(behaviour); } }
+    }
+}
---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/Loaders/LocalObjectSelector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Corp_Kaktus.MArenaEngine.Scripts.Network.RoleControl;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Network.Loaders
+{
+    public class LocalObjectSelector : MonoBehaviour
+    {
+        [Tooltip("true - instantiate need object. false - destroy not need object.")]
+        [SerializeField] private bool instantiate = true;
+        
+        [SerializeField] private GameObject onlyClientGameObject;
+        [SerializeField] private GameObject onlyConsoleGameObject;
+        [SerializeField] private GameObject onlyServerGameObject;
+        
+        private void Awake()
+        {
+            if (instantiate)
+            {
+                var selectObject = RoleController.currentRole switch
+                {
+                    Role.Client => onlyClientGameObject,
+                    Role.Console => onlyConsoleGameObject,
+                    Role.Server => onlyServerGameObject,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                if (selectObject != null)
+                {
+                    Instantiate(selectObject);
+                }
+            }
+            else
+            {
+                if (RoleController.currentRole != Role.Client && onlyClientGameObject ) { Destroy(onlyClientGameObject); }
+
+                if (RoleController.currentRole != Role.Console && onlyConsoleGameObject)
+                {
+                    Destroy(onlyConsoleGameObject);
+                }
+                if (RoleController.currentRole != Role.Server && onlyServerGameObject) { Destroy(onlyServerGameObject); }
+            }
+        }
+    }
+}
