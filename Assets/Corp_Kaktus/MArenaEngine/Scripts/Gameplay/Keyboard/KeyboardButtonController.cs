0b611983801998de0ca1c0279ce95d449f97d0ba
Djastas <109723332+Djastas@users.noreply.github.com>
2025-03-09 14:09:03 +0500

    add vr Keyboard

---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Gameplay/Keyboard/KeyboardButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Gameplay.Keyboard
+{
+    public class KeyboardButtonController : MonoBehaviour
+    {
+        public string character;
+        public UnityEvent<string> onPressed;
+        
+        public void Press() => onPressed?.Invoke(character);
+        
+        private void Awake()
+        {
+            onPressed.AddListener(GetComponentInParent<KeyboardController>().WriteChar);
+        }
+    }
+}
---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Gameplay/Keyboard/KeyboardController.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Gameplay.Keyboard
+{
+    public class KeyboardController : MonoBehaviour
+    {
+        private TMP_InputField _inputField;
+        
+        [SerializeField] private TMP_InputField keyboardInputField;
+        public void WriteChar(string character)
+        {
+            /*if (!_inputField || _inputField.gameObject !=  EventSystem.current.currentSelectedGameObject)
+            {
+                _inputField =  EventSystem.current.currentSelectedGameObject.GetComponent<InputField>();
+            }*/
+            keyboardInputField.text += character;
+        }
+    }
+}
