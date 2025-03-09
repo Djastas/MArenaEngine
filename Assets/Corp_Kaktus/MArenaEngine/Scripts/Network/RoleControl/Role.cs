037144a9f4a7054825d859fbeb84c93757093812
Djastas <109723332+Djastas@users.noreply.github.com>
2025-03-09 21:52:00 +0500

    add roles

---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/RoleControl/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Corp_Kaktus.MArenaEngine.Scripts.Network.RoleControl
+{
+    public enum Role
+    {
+        Client,
+        Server,
+        Console
+    }
+}
---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/RoleControl/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Multiplayer;
+using UnityEngine;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Network.RoleControl
+{
+    public static class RoleController
+    {
+        public static Role currentRole
+        {
+            get
+            {
+                if ((MultiplayerRolesManager.ActiveMultiplayerRoleMask & MultiplayerRoleFlags.Server) != 0)
+                {
+                    return Role.Server;
+                }
+                if ((MultiplayerRolesManager.ActiveMultiplayerRoleMask & MultiplayerRoleFlags.Client) != 0)
+                {
+                    if (EngineSettings.EngineSettings.settings.controlClientBuild)
+                    {
+                        return Role.Console;
+                    }
+                    if ((MultiplayerRolesManager.ActiveMultiplayerRoleMask & MultiplayerRoleFlags.Client) != 0)
+                    {
+                        return Role.Client;
+                    }
+                }
+                Debug.LogError("No Multiplayer Role!");
+                return Role.Client;
+            }
+        }
+    }
+
+    
+}
