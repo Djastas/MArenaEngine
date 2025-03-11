c5f9f26339273960765aa78819bf19cd7bb9e3a6
Djastas <109723332+Djastas@users.noreply.github.com>
2025-03-11 12:03:07 +0500

    add GetUnityTransport to utils

--- Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/Utils/Utils.cs
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/Utils/Utils.cs
@@ -9,17 +9,28 @@ namespace Corp_Kaktus.MArenaEngine.Scripts.Network.Utils
     {
         public static void SetIp(string ip)
         {
+            GetUnityTransport().ConnectionData.Address = ip;
+        }
+
+        public static UnityTransport GetUnityTransport()
+        {
             var temp = (NetworkManager.Singleton.NetworkConfig.NetworkTransport);
             try
             {
                 var unityTransport = (UnityTransport)temp;
-                unityTransport.ConnectionData.Address = ip;
+                return unityTransport;
             }
             catch (Exception e)
             {
                 Debug.LogError("Network transport is not UnityTransport!");
                 Debug.LogError(e);
+                throw;
             }
+        }
+
+        public static void SetPlayerPrefab(GameObject playerPrefab)
+        {
+            NetworkManager.Singleton.NetworkConfig.PlayerPrefab = playerPrefab;
         }
     }
 }
