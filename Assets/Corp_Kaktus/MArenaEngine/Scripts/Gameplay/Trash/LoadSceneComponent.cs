0d5f26dd295988f2c34800513597671ab551e7f7
Djastas <109723332+Djastas@users.noreply.github.com>
2025-03-11 10:56:02 +0500

    change CheckSettingsLoadLevel to loadScene
    use LocalObjectSelector to select needed sceneLoad

--- Assets/Corp_Kaktus/MArenaEngine/Scripts/Gameplay/Trash/LoadSceneComponent.cs
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Gameplay/Trash/LoadSceneComponent.cs
@@ -5,9 +5,17 @@ namespace Corp_Kaktus.MArenaEngine.Scripts.Gameplay.Trash
 {
     public class LoadSceneComponent : MonoBehaviour
     {
+        [SerializeField] private string sceneToLoad;
+        [SerializeField] private bool loadOnStart;
         public void LoadScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
+        }
+
+        private void Start()
+        {
+            if (loadOnStart)
+                LoadScene(sceneToLoad);
         }
     }
 }
