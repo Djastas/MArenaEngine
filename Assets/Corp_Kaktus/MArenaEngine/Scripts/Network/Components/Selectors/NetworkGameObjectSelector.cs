66eb99d5cf7980156cf5bdc94c2dec01fa49df83
Djastas <Timebrocop@gmail.com>
2025-04-23 20:24:54 +0500

    BugFix warrior head hinder view

---
+++ Assets/Corp_Kaktus/MArenaEngine/Scripts/Network/Components/Selectors/NetworkGameObjectSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Corp_Kaktus.MArenaEngine.Scripts.Network.Logger;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Corp_Kaktus.MArenaEngine.Scripts.Network.Components.Selectors
+{
+    [AddComponentMenu("Corp_Kaktus/MArenaEngine/Selectors/NetworkGameObjectSelector")]
+    public class NetworkGameObjectSelector : NetworkBehaviour
+    {
+        [SerializeField] private List<GameObject> onlyOwnerGameObjects;
+        [SerializeField] private List<GameObject> onlyNotOwnerGameObjects;
+
+        private void Start()
+        {
+            switch (IsOwner)
+            {
+                case false:
+                    DestroyAll(onlyOwnerGameObjects);
+                    break;
+                case true:
+                    DestroyAll(onlyNotOwnerGameObjects);
+                    break;
+            }
+            NetDebugger.instance.Log($"{OwnerClientId}:::{NetworkManager.LocalClientId}:::{IsOwner}" );
+        }
+
+        private static void DestroyAll(List<GameObject> gameObjects)
+        { foreach (var gameObject in gameObjects) { Destroy(gameObject); } }
+    }
+}
